ee8a094c06e74e96abf1e1f7937c2e5e1e28d2d5
renatus940804 <wonjun.s.dev@gmail.com>
2026-07-07 18:29:43 +0900

    feat(header): 검색 바 스크립트 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
 
@@ -24,4 +25,24 @@ function showBasket() {
 }
 function hideBasket() {
   basketEl.classList.remove('show');
+}
+
+// 검색
+const headerEl = document.querySelector('header');
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const shadowEl = searchWrapEl.querySelector('.shadow');
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+shadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+}
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
 }
