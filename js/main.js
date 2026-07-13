005f1990d2b6b74d58910b7093ba7bfc94a6a89d
renatus940804 <wonjun.s.dev@gmail.com>
2026-07-13 14:55:38 +0900

    feat(header): 반응형 레이아웃 적용  - 헤더 메뉴 버튼 토글과 배경 처리  - 헤더 검색 표시와 메뉴 목록 처리

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ searchShadowEl.addEventListener('click', hideSearch);
 
 function showSearch() {
   headerEl.classList.add('searching');
-  document.documentElement.classList.add('fixed');
+  stopScroll();
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
   });
@@ -59,7 +59,7 @@ function showSearch() {
 }
 function hideSearch() {
   headerEl.classList.remove('searching');
-  document.documentElement.classList.remove('fixed');
+  playScroll();
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
   });
@@ -69,6 +69,25 @@ function hideSearch() {
   searchDelayEls.reverse();
   searchInputEl.value = '';
 }
+function playScroll() {
+  document.documentElement.classList.remove('fixed');
+}
+function stopScroll() {
+  document.documentElement.classList.add('fixed');
+}
+
+// 헤더 메뉴 토글!
+const menuStarterEl = headerEl.querySelector('header .menu-starter');
+menuStarterEl.addEventListener('click', function () {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing');
+
+    playScroll();
+  } else {
+    headerEl.classList.add('menuing');
+    stopScroll();
+  }
+});
 
 // 요소의 가시성 관찰
 const io = new IntersectionObserver(function (entries) {
