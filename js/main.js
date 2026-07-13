2d4b3b9a436dcfff94ea83c9cd4b9e3b26f215aa
renatus940804 <wonjun.s.dev@gmail.com>
2026-07-13 19:32:28 +0900

    feat(footer): 반응형 레이아웃 적용  - 푸터 섹션  - 아코디언 메뉴

--- js/main.js
+++ js/main.js
@@ -206,6 +206,7 @@ navigations.forEach(function (nav) {
   mapEl.innerHTML = `
     <h3>
       <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
     </h3>
     <ul>
       ${mapList}
@@ -217,3 +218,11 @@ navigations.forEach(function (nav) {
 
 const thisYearEl = document.querySelector('span.this-year');
 thisYearEl.textContent = new Date().getFullYear();
+
+const mapEls = document.querySelectorAll('footer .navigations .map');
+mapEls.forEach(function (el) {
+  const h3El = el.querySelector('h3');
+  h3El.addEventListener('click', function () {
+    el.classList.toggle('active');
+  });
+});
